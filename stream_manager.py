3adca47f5b4efac763dd901285e5d283a8132a69
pkmksingh <picturesmk111@gmail.com>
2025-09-06 01:32:41 +0530

    Update stream_manager.py

--- stream_manager.py
+++ stream_manager.py
@@ -1,34 +1,28 @@
-# stream_manager.py (snippet)
 import threading
 from resilient_rtmp_streamer import ResilientStreamer
-import signal
 
 class StreamManager:
     def __init__(self):
         self.stream_threads = {}
-        self._stop_event = threading.Event()
-        signal.signal(signal.SIGTERM, self._on_term)
-        signal.signal(signal.SIGINT, self._on_term)
+        self._stop_events = {}
 
     def start_stream(self, stream_id, input_url, destinations):
         if stream_id in self.stream_threads:
-            # already running
-            return
-        streamer = ResilientStreamer(input_url, destinations, self._stop_event)
+            return  # already running
+
+        stop_event = threading.Event()
+        self._stop_events[stream_id] = stop_event
+        streamer = ResilientStreamer(input_url, destinations, stop_event)
         self.stream_threads[stream_id] = streamer
         streamer.start()
 
     def stop_stream(self, stream_id):
-        # set stop and wait briefly
-        # If you manage multiple streams independently you may want per-stream stop events
         if stream_id in self.stream_threads:
-            # with current design a global stop_event is used; for per-stream stopping replace with per-stream events
-            self._stop_event.set()
+            self._stop_events[stream_id].set()
             self.stream_threads[stream_id].join(timeout=5)
             del self.stream_threads[stream_id]
+            del self._stop_events[stream_id]
 
-    def _on_term(self, signum, frame):
-        self._stop_event.set()
-        # join all threads
-        for t in list(self.stream_threads.values()):
-            t.join(timeout=5)
+    def stop_all(self):
+        for sid in list(self.stream_threads.keys()):
+            self.stop_stream(sid)
